e57c998b269f04202f235345c3dae3e1cd711135
Tristangit config --global user.name Tristan <simon@Tristan.localdomain>
2025-10-16 19:15:36 +0200

    ???

--- test.asm
+++ test.asm
@@ -1,24 +1,28 @@
-;remote wsl 
 section .data
-    msg db "Hello", 0xA
-
+    Q db 0x51, 0x41, 0x41, 0x0A, 0x00
 section .text
-    global _start              ; main function
-
+    global _start
 
 _start:
-    mov rax, 1                 ; syscall: write
-    mov rdi, 1                 ; file descriptor 1 - stdout
-    lea rsi, [rel msg]         ; load ef adress of the ptr
-    mov rdx, 15                ; l
+    mov rax, 0x1                 ; syscall: write
+    mov rdi, 0x1                 ; file descriptor 1 = stdout
+    lea rsi, [rel Q]           ; lea Q
+    mov rdx, 0x5                 ; l = 5
     syscall
 
-    call test
+    sub rsp, 8                 ; stack + 0x8 negative
+    mov qword [rsp], 1         ; current rsp is a 64bit value that has the value of -> 1
+    call test                  ; call function
+    add rsp, 0x8                 ; Stack wieder freigeben
 
-    mov rax, 60                ; syscall: exit  
-    mov rdi, 0            ; ret 0
-    syscall
+    mov rax, 0x3C               
+    mov rdi, 0x0
+    db 0x0F 0x05 ;syscall 
 
-    
 test:
-    ret
+    push rbp
+    mov rbp, rsp
+    add qword [rbp+0x10], 4      ; qword rsp is at [rbp+16]
+    mov rax, 0x0
+    pop rbp                      ; = mov rsp, rbp / pop rbp
+    db 0xC3 ;ret
