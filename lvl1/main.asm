57959445b0803643dad5db94f9659ad12894e97a
Your Name <you@example.com>
2026-02-13 19:10:21 +0100

    --allow-empty

---
+++ lvl1/main.asm
@@ -0,0 +1,6 @@
+global lvl01
+lvl01:
+    mov rax, rdi
+    imul rax, rsi
+    add rax, rdx
+    ret
---
+++ lvl1/main.cpp
@@ -0,0 +1,6 @@
+int lvl1(long arg1, long arg2, long arg3) {
+	long x = arg1;
+	x*=arg2;
+	x+=arg3;
+	return x;
+}
